170c918c96444d0806eafcc12993fe8511d1cc8f
Trix-ctrl <greatokpeke505@gmail.com>
2024-11-18 23:27:28 +0100

    Add files via upload

---
+++ Great.py
@@ -0,0 +1,29 @@
+def a():
+    print("you have chosen to perform multiplication")
+    num1 =float(input("input your first digit:"))
+    num2 =float(input("input your second digit:"))
+    print(num1*num2)
+
+def b():
+    print("you have chosen to perform division")
+    num1 =float(input("input your first digit:"))
+    num2 =float(input("input your second digit:"))
+    print(num1/num2)
+
+def c():
+    print("you have chosen to perform subtraction")
+    num1 =float(input("input your first digit:"))
+    num2 =float(input("input your second digit:"))
+    print(num1-num2)    
+
+def d():
+    print("you have chosen to perform addition")
+    num1 =float(input("input your first digit:"))
+    num2 =float(input("input your second digit:"))
+    print(num1+num2)
+
+def e():
+    print("you have chosen to perform raised to the power of")
+    num1 =float(input("input your first digit:"))
+    num2 =float(input("input your second digit:"))
+    print(num1^num2)
